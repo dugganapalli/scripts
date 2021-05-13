0a9e6cbb694d0bc760b59baf7c3c305cabba9478
venkatarami reddy duggnapalli <venkatarami.reddy@flipkart.com>
2021-05-13 09:01:17 +0530

    scripts

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# scripts
+# Some_Scripts
---
+++ create_docker_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+logfile=dockerimage.log
+
+cat > dockerfile << EOF
+FROM docker.io/python:latest
+MAINTAINER selvakumar0710@gmail.com
+RUN apt-get update && apt-get -y upgrade python
+EOF
+
+docker build -t mypython:latest . > $logfile
+echo "Logs of docker image creations are stored in the file : $logfile"
